2c83e90f08cd1fcc306b639cc08eeb55a8f00649
cameronpadua <cameron.padua@gmail.com>
2018-05-20 18:13:24 -0700

    Added fullyConneceted function

--- fileMaker.py
+++ fileMaker.py
@@ -1,3 +1,4 @@
+import numpy as np
 # -*- coding: utf-8 -*-
 def line(maxNode):
     fileName = "line"+str(maxNode)+".txt"
@@ -10,7 +11,26 @@ def line(maxNode):
     print("Line File made. File is named ", fileName)
     
 def fullConnected(maxNode):
-    pass
+    record = np.zeros((maxNode+1,maxNode+1),dtype=bool)
+    fileName = "fullyConnected"+str(maxNode)+".txt"
+    File = open(fileName,"w")
+    
+    for x in range(1,maxNode):
+    #create ring of values:
+        writePairs(File, x, x+1)
+        record[x, x+1] = True;
+        record[x+1, x] = True;
+        record[x, x] = True;
+    record[maxNode, maxNode] = True;
+    for y in range(1,maxNode+1):
+        for x in range(1,maxNode+1):
+            if(record[y, x] == False):
+                writePairs(File, x, y)
+                record[y, x] = True;
+                record[x, y] = True;
+                
+    File.close()
+    print("Fully connected File made. File is named ", fileName)
     
 def mesh(maxNode):
     pass
@@ -65,3 +85,4 @@ def main(maxNumber):
     ring(maxNumber)
     star(maxNumber)
     tree(maxNumber)
+    fullConnected(maxNumber)
