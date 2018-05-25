8ff0383e066668fa72d8742f14cb78a9fe098462
cameronpadua <cameron.padua@gmail.com>
2018-05-24 22:12:08 -0700

    added a logic bomb virus

--- testSimulation.py
+++ testSimulation.py
@@ -4,7 +4,12 @@ import viruses
 import Network as n
 
 
-virus = viruses.SuperVirus()
+# virus = viruses.superVirus()
+# theNetwork = n.Network(disp.graphType.MESH)
+# theNetwork.createnetwork("mesh.txt")
+# sim.runOnce(theNetwork, 1, virus)
+
+virus = viruses.logicBomb()
 theNetwork = n.Network(disp.graphType.MESH)
 theNetwork.createnetwork("mesh.txt")
 sim.runOnce(theNetwork, 1, virus)
--- viruses.py
+++ viruses.py
@@ -1,4 +1,5 @@
 import numpy as N
+import time
 
 """ this is an example virus, it has a 50% chance of infecting any neighboring viruses.
     Model all future viruses after this one, and make sure they have a function called 
@@ -9,3 +10,17 @@ class SuperVirus:
   def infectOrNot(self, network, nodeID):
     retVal = N.random.uniform() 
     return retVal < self.chance
+class logicBomb:
+    infectedCount = 0;
+    def infectOrNot(self, network, nodeID):
+        #get current time in milliseconds
+        ms = int(round(time.time() * 1000))
+        #Always infect if the network is 75% infected by this virus
+        if(self.infectedCount>int(len(network.edges)*.75)):
+            return True
+        #of infect if the current time modulus 5 is 0
+        elif(ms % 5 == 0):
+            self.infectedCount+=1
+            return True
+        return False
+        
