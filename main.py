fedcf9d88f993f40c147dc03503de35a6d69e16b
sandunr <sandunr@gmail.com>
2018-05-26 11:11:50 -0700

    changes to main

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 # -*- coding: utf-8 -*-
 import numpy as N
 from Network import Network
+import viruses
 
 filename = 'mesh.txt'
 network = Network('mesh')
 network.createnetwork(filename)
 print(network.edges)
+
+virus = viruses.SuperVirus()
+
+numIter = 100    # represents 100 days
+
+rand = int(N.random.uniform() * len(network.nodes.keys()))
+while rand is 0:
+    rand = int(N.random.uniform() * len(network.nodes.keys()))
+
+network.nodes[rand].state = Network.State.infected
+network.infectedList.append(network.nodes[rand].nodeID)
+
+state = []
+state.append(network.infectedList)
+
+for i in range(numIter):
+    for infectedNode in network.infectedList:
+        adjNodes = network.nodes[infectedNode].adjacentNodes
