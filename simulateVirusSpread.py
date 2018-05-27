b0fc2408a28fb010bae5489b1f5a860d1b69a0f1
dla0267 <dla0267@gmail.com>
2018-05-27 12:05:31 -0700

    graph function added and runOnce func modified

--- simulateVirusSpread.py
+++ simulateVirusSpread.py
@@ -19,6 +19,10 @@ def runOnce(network, startingPoint, virus, displayAnimation = True):
   displayData = disp.dataToDisplay()
   displayData.typeOfGraph = network.networkType
 
+  # list of percentage of infected systems in network
+  percent_infected = []
+  percent_infected.append(percentage(network)) # initial percentage before simulation
+
   while simulationNotOver == True:
     hasNewAnimationInfo = False
     currentTurnMoves = []
@@ -35,7 +39,22 @@ def runOnce(network, startingPoint, virus, displayAnimation = True):
       network.infectedList[move[1]] = n.state.infected
     if hasNewAnimationInfo:
       displayData.animationSteps.append(currentTurnMoves)
+      
+      percent_infected.append(percentage(network))
     
     if nowhereToGo(network):
       simulationNotOver = False  
   disp.display(displayData)
+  drawGraph(displayData.animationSteps, percent_infected)
+  #return displayData.animationSteps
+
+
+# function to calculate the infection percentage of network. 
+def percentage(network):
+  return len(network.infectedList) / len(network.node) * 100
+
+
+# draw a xy plot that shows the percentage of infected system in network per step 
+def drawGraph(time_turns, percentage):
+    fig1 = plt.figure() # plot time turns vs percentage of infected network/system
+    fig1.plot(time_turns, percentage)
