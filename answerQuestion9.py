733816d2115436f2a71ccf16625e1598cea59cd7
sandunr <sandunr@gmail.com>
2018-06-01 06:23:14 -0700

    Update answerQuestion9.py

--- answerQuestion9.py
+++ answerQuestion9.py
@@ -6,15 +6,25 @@ import fileMaker as fm
 import numpy as N
 import matplotlib.pyplot as plt
 
-
-time_taken_Trojan = []
+# Store time for each run
+time_taken_Trojan = []  
 time_taken_Worm = []
 
+# Node strength range is divided into 3 categories
 node_strengths = [(0, 0.2), (0.2, 0.4), (0.4, 0.6)]
 strength_scale = ['High', 'Medium', 'Low']
 
+# Simulation for the Trojan
 virus = viruses.Trojan()
 for i in range(len(node_strengths)):
+    """
+        For the length of the node_strenths array, each time a network for each
+        type of network is created consisting of 50 nodes. Then runOnce() method of
+        simulateVirusSpread module is called which returns the time it took to
+        run the simulation, infection rates, and percent of infected nodes in the
+        network. The times taken are appended to the time_taken_Trojan array each
+        iteration.
+    """
     fm.main(50)
     line_Network = n.Network(disp.graphType.LINE)
     line_Network.createnetwork("line.txt", node_strengths[i])
@@ -29,6 +39,7 @@ for i in range(len(node_strengths)):
     mesh_Network = n.Network(disp.graphType.MESH)
     mesh_Network.createnetwork("mesh.txt", node_strengths[i])
 
+    # Call runOnce in the simulateVirusSpread module for each network type
     time_turns1,infectionRates1, percent_infected_line = sim.runOnce(line_Network, 1, virus, False)
     time_turns2,infectionRates2, percent_infected_ring = sim.runOnce(ring_Network, 1, virus, False)
     time_turns3,infectionRates3, percent_infected_star = sim.runOnce(star_Network, 1, virus, False)
@@ -45,10 +56,11 @@ for i in range(len(node_strengths)):
 
 # rows = size of network
 # columns = shape of network
-time_taken_Trojan = N.reshape(time_taken_Trojan, [3, 6])
+time_taken_Trojan = N.reshape(time_taken_Trojan, [3, 6])  # Reshape to the correct width/rows
 print(time_taken_Trojan)
 #
 # network data depending on the size of network 10 - 100 for each shape of network
+# Get the corresponding portion of the time_taken_Trojan for each type of network.
 line_data_Trojan = N.array(time_taken_Trojan[:, 0])
 ring_data_Trojan = N.array(time_taken_Trojan[:, 1])
 star_data_Trojan = N.array(time_taken_Trojan[:, 2])
@@ -72,6 +84,14 @@ plt.show()
 
 virus = viruses.Worm()
 for i in range(len(node_strengths)):
+    """
+        For the length of the node_strenths array, each time a network for each
+        type of network is created consisting of 50 nodes. Then runOnce() method of
+        simulateVirusSpread module is called which returns the time it took to
+        run the simulation, infection rates, and percent of infected nodes in the
+        network. The times taken are appended to the time_taken_Worm array each
+        iteration.
+    """
     fm.main(50)
     line_Network = n.Network(disp.graphType.LINE)
     line_Network.createnetwork("line.txt", node_strengths[i])
@@ -86,6 +106,7 @@ for i in range(len(node_strengths)):
     mesh_Network = n.Network(disp.graphType.MESH)
     mesh_Network.createnetwork("mesh.txt", node_strengths[i])
 
+    # Call runOnce in the simulateVirusSpread module for each network type
     time_turns1,infectionRates, percent_infected = sim.runOnce(line_Network, 1, virus, False)
     time_turns2,infectionRates, percent_infected = sim.runOnce(ring_Network, 1, virus, False)
     time_turns3,infectionRates, percent_infected = sim.runOnce(star_Network, 1, virus, False)
@@ -105,15 +126,16 @@ for i in range(len(node_strengths)):
 time_taken_Worm = N.reshape(time_taken_Worm, [3, 6])
 print(time_taken_Worm)
 
-# network data depending on the size of network 10 - 100 for each shape of network
-# line_data_Worm = N.array(time_taken_Worm[:, 0])
-# ring_data_Worm = N.array(time_taken_Worm[:, 1])
-# star_data_Worm = N.array(time_taken_Worm[:, 2])
-# tree_data_Worm = N.array(time_taken_Worm[:, 3])
-# fullconnect_data_Worm = N.array(time_taken_Worm[:, 4])
-# mesh_data_Worm = N.array(time_taken_Worm[:, 5])
+network data depending on the size of network 10 - 100 for each shape of network
+Get the corresponding portion of the time_taken_Trojan for each type of network.
+line_data_Worm = N.array(time_taken_Worm[:, 0])
+ring_data_Worm = N.array(time_taken_Worm[:, 1])
+star_data_Worm = N.array(time_taken_Worm[:, 2])
+tree_data_Worm = N.array(time_taken_Worm[:, 3])
+fullconnect_data_Worm = N.array(time_taken_Worm[:, 4])
+mesh_data_Worm = N.array(time_taken_Worm[:, 5])
 #
-# # drawing graph to compare
+# drawing graph to compare
 # fig = plt.figure()
 # plt.plot(strength_scale, line_data_Worm)
 # plt.plot(strength_scale, ring_data_Worm)
